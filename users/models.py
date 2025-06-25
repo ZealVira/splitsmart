b748adfd3070cdc7a98cc1cd866f282b920fdc1e
zealvira <zeal.vira@somaiya.edu>
2025-06-26 03:42:43 +0530

    login and register pages

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=False, blank=True, null=True)
+    email = models.EmailField(_('email address'), unique=True)
+    full_name = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  
+
+    def __str__(self):
+        return self.email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib.auth import authenticate, login 
+from django.contrib import messages
 
+def index(request):
+    return render(request, 'index.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confpassword']
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return render(request, 'signup.html')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email already exists.")
+            return render(request, 'signup.html')
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        messages.success(request, "Account created successfully. Please log in.")
+        print("User created successfully")
+        return redirect('login')
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            messages.error(request, "Invalid email or password.")
+            print("Invalid credentials")
+
+    return render(request, 'login.html')
 # Create your views here.
